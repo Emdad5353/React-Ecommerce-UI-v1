a1aaccce92c0bc046b1a2cea8a7e9de06c4df270
Emdad5353 <myselfemdad@gmail.com>
2022-08-23 02:31:32 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import { Routes, Route } from "react-router-dom";
+import Cart from "./pages/Cart";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Product from "./pages/Product";
+import ProductList from "./pages/ProductList";
+import Register from "./pages/Register";
+
+const App = () => {
+  return (
+    <>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/productlist' element={<ProductList />} />
+        <Route path='/product' element={<Product />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/register' element={<Register />} />
+        <Route path='/cart' element={<Cart />} />
+      </Routes>
+    </>
+  );
+};
+
+export default App;
